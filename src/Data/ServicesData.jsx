5b6b6d0b9ebb673a50ef79d32145b19c4ff7cde1
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-12-30 00:51:40 +0600

    add card description

--- src/Data/ServicesData.jsx
+++ src/Data/ServicesData.jsx
@@ -1,77 +1,92 @@
-export const Services = [
+export const ServicesData = [
     {
         id:1,
         name: "Maid",
+        desc:"an unmarried girl or woman especially when young",
         img: "https://i.ibb.co/f0Dn90H/maid.jpg"
     },
     {
         id:2,
         name: "Nurse",
+        desc:" a person who cares for the sick or infirm",
         img: "https://i.ibb.co/Rjtm6fC/nurse.jpg"
     },
     {
         id:3,
         name: "Governess",
+        desc:"a woman employed as a private tutor",
         img: "https://i.ibb.co/zG8B9TF/governess.webp"
     },
     {
         id:4,
         name: "Cook",
+        desc:"a person whose job is to prepare and cook food",
         img: "https://i.ibb.co/25CQQfw/cook.jpg"
     },
     {
         id:5,
         name: "Babysitter",
+        desc:"a person hired to take care of a child or children",
         img: "https://i.ibb.co/2jzQ091/babysitter1.jpg"
     },
     {
         id:6,
         name: "Driver",
+        desc:"a person who drives a vehicle",
         img: "https://i.ibb.co/4gv0X52/driver.jpg"
     },
     {
         id:7,
         name: "Laborer",
+        desc:"a person who works in manual labor types in the construction industry workforce",
         img: "https://i.ibb.co/Jpvx8Bb/laborer.jpg"
     },
     {
         id:8,
         name: "Builder",
+        desc:"a person whose job is to build or repair houses and other buildings",
         img: "https://i.ibb.co/z8wDTHj/builder.jpg"
     },
     {
         id:9,
         name: "Painter",
+        desc:"A painter is an artist who paints pictures.",
         img: "https://i.ibb.co/VgDw16S/painter.jpg"
     },
     {
         id:10,
         name: "Electrician",
+        desc:"A tradesman who installs and repairs electrical wiring and equipment.",
         img: "https://i.ibb.co/6FCM1dx/electrician.jpg"
     },
     {
         id:11,
         name: "Carpenter",
+        desc:"a worker who builds or repairs wooden structures or their structural parts.",
         img: "https://i.ibb.co/fdjqvHx/carpenter.jpg"
     },
     {
         id:12,
         name: "Plumber",
+        desc:"a person whose job is to connect and repair things such as water and drainage pipes, baths, and toilets.",
         img: "https://i.ibb.co/ZBPcH8W/plumber.jpg"
     },
     {
         id:13,
         name: "Night Guard",
+        desc:"An occlusal splint is commonly called a dental guard, night guard or bite guard.",
         img: "https://i.ibb.co/0GBT8NF/night-guard.webp"
     },
     {
         id:14,
         name: "Designer",
+        desc:"a person who plans the form or structure of something before it is made, by preparing drawings or plans.",
         img: "https://i.ibb.co/tHzXVQZ/designer.jpg"
     },
     {
         id:15,
         name: "Developer",
+        desc:"an individual that builds and create software and applications.",
         img: "https://i.ibb.co/j5s082L/developer.jpg"
     },
 
--- src/Pages/Services/ServicesCard.jsx
+++ src/Pages/Services/ServicesCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ServicesData } from '../../Data/ServicesData';
 
 const ServicesCard = () => {
     return (
@@ -9,44 +10,49 @@ const ServicesCard = () => {
                         The leading global marketplace <br />
                         for hiring people
                     </h2>
-                    <div className="grid md:grid-cols-2 grid-cols-1 my-14 md:mx-4 sm:mx-4 gap-y-14 ">
-                        <div className="flex  items-center justify-around sm:flex-col md:flex-row gap-4 lg:gap-0">
-                            <div className="md:w-40 lg:w-60 w-3/4 lg:h-60 md:h-40 h-72 lg:flex-shrink-0 md:flex-shrink-0   bg-transparent cursor-pointer group perspective">
-                                <div className="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000 object-contain">
-                                    <div className="absolute backface-hidden   w-full h-full">
-                                        <div className="h-full w-full relative">
-                                            <img
-                                                src="https://themes-themegoods.b-cdn.net/coursector/wp-content/uploads/2019/04/student-friends-studying-together-UNCHR5E-768x513.jpg"
-                                                className="w-full h-full rounded-lg " alt=''
-                                            />
-                                            <div className="w-full h-full absolute top-0 bg-opacity-20 rounded-lg bg-black">
-                                                <img
-                                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/VisualEditor_-_Icon_-_Search-big_-_white.svg/1200px-VisualEditor_-_Icon_-_Search-big_-_white.svg.png"
-                                                    alt="dd"
-                                                    className="shadow w-1/3 h-1/3 absolute top-[33%] left-[33%] bg-opacity-70 "
-                                                />
-                                                <h1 style={{ fontFamily: "Rajdhani" }} className="text-white text-3xl p-5 font-bold font-mono">
-                                                    Maid
-                                                </h1>
+                    <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 my-14 md:mx-4 sm:mx-4 gap-y-14 ">
+                        {
+                            ServicesData?.map(service => (
+                                <div key={service?.id} className="flex  items-center justify-around sm:flex-col md:flex-row gap-4 lg:gap-0">
+                                    <div className="md:w-40 lg:w-60 w-3/4 lg:h-60 md:h-40 h-72 lg:flex-shrink-0 md:flex-shrink-0   bg-transparent cursor-pointer group perspective">
+                                        <div className="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000 object-contain">
+                                            <div className="absolute backface-hidden   w-full h-full">
+                                                <div className="h-full w-full relative">
+                                                    <img
+                                                        src={service?.img}
+                                                        className="w-full h-full rounded-lg " alt=''
+                                                    />
+                                                    <div className="w-full h-full absolute top-0 bg-opacity-20 rounded-lg bg-black">
+                                                        <img
+                                                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/VisualEditor_-_Icon_-_Search-big_-_white.svg/1200px-VisualEditor_-_Icon_-_Search-big_-_white.svg.png"
+                                                            alt="dd"
+                                                            className="shadow w-1/3 h-1/3 absolute top-[33%] left-[33%] bg-opacity-70 "
+                                                        />
+                                                        <h1 style={{ fontFamily: "Rajdhani" }} className="text-white shadow-md text-3xl p-5 font-bold font-mono">
+                                                            {service?.name}
+                                                        </h1>
+                                                    </div>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </div>
-                                    <div className="absolute my-rotate-y-180 backface-hidden rounded-lg w-full h-full bg-gray-300 overflow-hidden">
-                                        <div className="h-full w-full relative">
-                                            {/* <img src="https://themes-themegoods.b-cdn.net/coursector/wp-content/uploads/2019/04/creative-business-people-working-on-business-WGCU95A-768x512.jpg"
-                                                className="w-full h-full" alt=''
-                                            /> */}
-                                            <div className="w-full h-full absolute top-0 bg-opacity-30 rounded-lg bg-black flex flex-col space-y-2 items-center justify-center">
-                                                <h1 style={{ fontFamily: "Rajdhani" }} className="text-white text-3xl font-bold font-mono">
-                                                    Maid
-                                                </h1>
-                                                <button className="btn btn-outline outline-primary px-7 ">Click to Hire</button>
+                                            <div className="absolute hover:shadow-lg my-rotate-y-180 backface-hidden rounded-lg w-full h-full bg-gray-700 overflow-hidden">
+                                                <div className="h-full w-full relative">
+                                                    {/* <img src="https://themes-themegoods.b-cdn.net/coursector/wp-content/uploads/2019/04/creative-business-people-working-on-business-WGCU95A-768x512.jpg"
+                                                    className="w-full h-full" alt=''
+                                                    /> */}
+                                                    <div className="w-full h-full absolute top-0 bg-opacity-30 rounded-lg bg-black flex flex-col space-y-2 items-center justify-center">
+                                                        <h1 style={{ fontFamily: "Rajdhani" }} className="text-info text-3xl font-bold font-mono">
+                                                            {service?.name}
+                                                        </h1>
+                                                        <p className="text-sm text-white">{service?.desc}</p>
+                                                        <button className="btn btn-outline btn-info px-7 ">Click to Hire</button>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
+                            ))
+                        }
                     </div>
                 </div>
             </div>
