f65ac9cc6426c59351e2df37baab2e3be7062748
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-12-29 18:02:12 +0600

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import SignIn from './Pages/Authentication/SignIn/SignIn';
+import Contact from './Pages/Contact/Contact';
+import Home from './Pages/Home/Home';
+import NotFound from './Pages/NotFound/NotFound';
+import Services from './Pages/Services/Services';
+import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/services' element={<Services/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/signin' element={<SignIn/>} />
+        <Route path='*' element={<NotFound/>} />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='w-full h-full items-center justify-center'>
+            <h2 className="text-5xl text-rose-600 font-bold">This is Home Page</h2>
+        </div>
+    );
+};
+
+export default Home;
