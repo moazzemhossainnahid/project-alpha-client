7c8753b0548e90b371b1c157cb63faff90c507ef
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-12-29 19:00:06 +0600

    set navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,38 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        my_theme: {
+          "primary": "#047857",
+          "secondary": "#16a34a",
+          "accent": "#1FB2A6",
+          "neutral": "#191D24",
+          "base-100": "#ffffff",
+          "info": "#e11d48", 
+          "success": "#36D399",   
+          "warning": "#FBBD23",   
+          "error": "#F87272",
+        },
+      },
+      {
+        my_dark: {
+          "primary": "#047857",
+          "secondary": "#16a34a",
+          "accent": "#1FB2A6",
+          "neutral": "#191D24",
+          "base-100": "#ffffff",
+          "info": "#e11d48", 
+          "success": "#36D399",   
+          "warning": "#FBBD23",   
+          "error": "#F87272",
+        },
+      },
+      "light",
+      "dracula",
+    ],
   },
-  plugins: [],
-};
+  plugins: [require("daisyui")],
+}
