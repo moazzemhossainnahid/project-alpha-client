bae61968003938dc15052941fdae850bea2c1ed6
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-01-10 20:21:13 +0600

    add new service data

---
+++ src/Data/ServicesNewData.jsx
@@ -0,0 +1,176 @@
+export const ServicesNewData = [
+    {
+        id: 1,
+        name: "Maid",
+        desc: "an unmarried girl or woman especially when young",
+        img: "https://i.ibb.co/f0Dn90H/maid.jpg",
+        provider: [
+            {
+                name: "Munira Aktar",
+                exp: 2,
+                idNo: "M0001",
+                phone: "+8801774XXXX04",
+                rating: 4
+            },
+            {
+                name: "Sahazana Katun",
+                exp: 4,
+                idNo: "M0002",
+                phone: "+8801774XXXX04",
+                rating: 5
+            },
+            {
+                name: "Nazmun Nahar",
+                exp: 2,
+                idNo: "M0003",
+                phone: "+8801774XXXX04",
+                rating: 3
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "Nurse",
+        desc: " a person who cares for the sick or infirm",
+        img: "https://i.ibb.co/Rjtm6fC/nurse.jpg",
+        provider: [
+            {
+                name: "Nishi Dikha",
+                exp: 3,
+                idNo: "N0001",
+                phone: "+8801774XXXX04",
+                rating: 4
+            },
+            {
+                name: "Rifat Mishu",
+                exp: 5,
+                idNo: "N0002",
+                phone: "+8801774XXXX04",
+                rating: 5
+            },
+            {
+                name: "Jui Tasnim",
+                exp: 2,
+                idNo: "N0003",
+                phone: "+8801774XXXX04",
+                rating: 2
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "Governess",
+        desc: "a woman employed as a private tutor",
+        img: "https://i.ibb.co/zG8B9TF/governess.webp",
+        provider: [
+            {
+                name: "Sharjina Asha",
+                exp: 3,
+                idNo: "G0001",
+                phone: "+8801774XXXX04",
+                rating: 5
+            },
+            {
+                name: "Nasiba Khan",
+                exp: 5,
+                idNo: "G0002",
+                phone: "+8801774XXXX04",
+                rating: 3
+            },
+            {
+                name: "Tuba Fardin",
+                exp: 2,
+                idNo: "G0003",
+                phone: "+8801774XXXX04",
+                rating: 1
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: "Cook",
+        desc: "a person whose job is to prepare and cook food",
+        img: "https://i.ibb.co/25CQQfw/cook.jpg",
+        provider: [
+            {
+                name: "Mithun Hasan",
+                exp: 3,
+                idNo: "C0001",
+                phone: "+8801774XXXX04",
+                rating: 4
+            },
+            {
+                name: "Morium Begum",
+                exp: 5,
+                idNo: "C0002",
+                phone: "+8801774XXXX04",
+                rating: 5
+            },
+            {
+                name: "Ferdus Ara",
+                exp: 1,
+                idNo: "C0003",
+                phone: "+8801774XXXX04",
+                rating: 1
+            }
+        ]
+    },
+    {
+        id:5,
+        name: "Babysitter",
+        desc:"a person hired to take care of a child or children",
+        img: "https://i.ibb.co/2jzQ091/babysitter1.jpg",
+        provider: [
+            {
+                name: "Nabila Zannat",
+                exp: 3,
+                idNo: "B0001",
+                phone: "+8801774XXXX04",
+                rating: 4
+            },
+            {
+                name: "Piyushi Thakur",
+                exp: 5,
+                idNo: "B0002",
+                phone: "+8801774XXXX04",
+                rating: 5
+            },
+            {
+                name: "Shamima Kobir",
+                exp: 1,
+                idNo: "B0003",
+                phone: "+8801774XXXX04",
+                rating: 2
+            }
+        ]
+    },
+    {
+        id:6,
+        name: "Driver",
+        desc:"a person who drives a vehicle",
+        img: "https://i.ibb.co/4gv0X52/driver.jpg",
+        provider: [
+            {
+                name: "Zikrul Apon",
+                exp: 3,
+                idNo: "D0001",
+                phone: "+8801774XXXX04",
+                rating: 4
+            },
+            {
+                name: "Umme Kulsum",
+                exp: 5,
+                idNo: "D0002",
+                phone: "+8801774XXXX04",
+                rating: 5
+            },
+            {
+                name: "Rifat Rosid",
+                exp: 1,
+                idNo: "D0003",
+                phone: "+8801774XXXX04",
+                rating: 2
+            }
+        ]
+    },
+]
