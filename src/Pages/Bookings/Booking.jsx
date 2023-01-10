153b2c707eb12debe09e7cc4e5135591a77d7eaf
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-01-11 01:27:35 +0600

    design booking

--- src/Pages/Bookings/Booking.jsx
+++ src/Pages/Bookings/Booking.jsx
@@ -4,29 +4,28 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../Firebase/firebase.init';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBangladeshiTakaSign } from '@fortawesome/free-solid-svg-icons';
-import { ServicesNewData } from '../../Data/ServicesNewData';
+import { faDollarSign } from '@fortawesome/free-solid-svg-icons';
 import useServices from '../../Hooks/useServices';
 
 const Booking = () => {
     const { id, srvid } = useParams();
     const [totalPrice, setTotalPrice] = useState();
     const [isChecked, setIsChecked] = useState(false);
-    const {data} = useServices();
+    const { data } = useServices();
     // const [srv, setSrv] = useState();
     const [user] = useAuthState(auth);
-  
-  console.log(data);
+
+    console.log(data);
     const handleChecked = (event) => {
-      if (event.target.checked) {
-        setIsChecked(true);
-      } else {
-        setIsChecked(false);
-      }
+        if (event.target.checked) {
+            setIsChecked(true);
+        } else {
+            setIsChecked(false);
+        }
     };
 
     const Services = data?.data?.find(s => s?.id === Number(srvid));
-    const singleData = Services?.provider?.find(s => s?.id === Number(id) );
+    const singleData = Services?.provider?.find(s => s?.id === Number(id));
     // console.log(id, srvid);
     console.log(Services);
     console.log(singleData);
@@ -59,7 +58,7 @@ const Booking = () => {
 
         console.log(info);
 
-    axios.post(`https://availbox-server.vercel.app/init`, info)
+        axios.post(`https://availbox-server.vercel.app/init`, info)
             .then(res => {
                 console.log(res);
                 if (res?.data) {
@@ -71,20 +70,30 @@ const Booking = () => {
 
     return (
         <div className='flex flex-col py-10 gap-7 px-4'>
-            <div className='flex flex-col md:flex-row p-2 rounded justify-between align-middle shadow-md px-12 my-auto'>
-                <img className='h-28 rounded' src={singleData?.img} alt=" " />
-                <div style={{ fontFamily: "Rajdhani" }} className='my-auto text-2xl font-bold flex flex-col gap-2'>
-                    <h4>{singleData?.name}</h4>
-                    <p><FontAwesomeIcon icon={faBangladeshiTakaSign} /> {singleData?.price?.perMonth}/Month</p>
+            <div className='flex flex-col p-2 rounded justify-between align-middle shadow-md px-12 my-auto'>
+                <div className="flex flex-col md:flex-row gap-3 w-full md:w-4/5 mx-auto items-center justify-center">
+                    <div className="w-full md:w-2/5">
+                        <img className='h-28 w-28 rounded-full' src={singleData?.img} alt=" " />
+                    </div>
+                    <div style={{ fontFamily: "Rajdhani" }} className='my-auto w-full md:w-3/5 text-2xl font-bold flex flex-col gap-2'>
+                        <h4 className='text-rose-600 text-2xl md:text-5xl'>{singleData?.name}</h4>
+                        <div className="text-end">
+                        <p><FontAwesomeIcon icon={faDollarSign} /> {singleData?.perHour}/Hour</p>
+                        <p><FontAwesomeIcon icon={faDollarSign} /> {singleData?.perDay}/Day</p>
+                        <p><FontAwesomeIcon icon={faDollarSign} /> {singleData?.perMonth}/Month</p>
+                        </div>
+                    </div>
                 </div>
-                <div className='my-auto'>
-                    <input onChange={handleInputValue}
-                        onLoad={(e) => e.target.value = 1}
-                        placeholder="Enter Quantity"
-                        className='input focus:outline-none input-bordered w-4/5' type="text" name="quantity" id="quantity" />
-                </div>
-                <div className='my-auto'>
-                    <h4 style={{ fontFamily: "Rajdhani" }} className='text-cyan-500 text-xl font-bold'> <FontAwesomeIcon icon={faBangladeshiTakaSign} /> {totalPrice ? singleData?.price * totalPrice : singleData?.price}</h4>
+                <div className="flex flex-col md:flex-row gap-3 w-full md:w-4/5 mx-auto items-center justify-center">
+                    <div className='w-full md:w-3/5 mx-auto'>
+                        <input onChange={handleInputValue}
+                            onLoad={(e) => e.target.value = 1}
+                            placeholder="Enter Quantity"
+                            className='input focus:outline-none input-bordered w-4/5' type="text" name="quantity" id="quantity" />
+                    </div>
+                    <div className='w-full md:w-2/5 mx-auto'>
+                        <h4 style={{ fontFamily: "Rajdhani" }} className='text-cyan-500 text-xl font-bold'> <span className="font-bold">Total: </span> <FontAwesomeIcon icon={faDollarSign} /> {totalPrice ? singleData?.price * totalPrice : singleData?.price}</h4>
+                    </div>
                 </div>
             </div>
             <div className='w-full mx-auto'>
@@ -152,6 +161,34 @@ const Booking = () => {
                                                 />
                                                 <span className=" text-sm bg-transparent te text-opacity-80 absolute left-0 px-2 top-[-37px] transition duration-200 input-text">
                                                     Service Price
+                                                </span>
+                                            </label>
+                                        </div>
+                                        <div className='mt-3'>
+                                            <label className="relative cursor-pointer">
+                                                <input
+                                                    type="text"
+                                                    placeholder="Enter Your Contact Number"
+                                                    required
+                                                    className="h-[50px]  bg-[#f3f3f3] w-full px-6 text-md border rounded outline-none focus:border-gray-700 focus:border-opacity-60 placeholder-gray-400 placeholder-opacity-1 transition duration-200"
+                                                    name="name"
+                                                />
+                                                <span className=" text-sm bg-transparent te text-opacity-80 absolute left-0 px-2 top-[-37px] transition duration-200 input-text">
+                                                    Contact Number
+                                                </span>
+                                            </label>
+                                        </div>
+                                        <div className='mt-3'>
+                                            <label className="relative cursor-pointer">
+                                                <input
+                                                    type="text"
+                                                    placeholder="Enter Your Contact Number"
+                                                    required
+                                                    className="h-[50px]  bg-[#f3f3f3] w-full px-6 text-md border rounded outline-none focus:border-gray-700 focus:border-opacity-60 placeholder-gray-400 placeholder-opacity-1 transition duration-200"
+                                                    name="price"
+                                                />
+                                                <span className=" text-sm bg-transparent te text-opacity-80 absolute left-0 px-2 top-[-37px] transition duration-200 input-text">
+                                                    Address
                                                 </span>
                                             </label>
                                         </div>
